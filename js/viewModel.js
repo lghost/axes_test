4db091c4f2b07b55ae69a68e7dcf5705d0e621f4
Vitaliy V <neural.null@gmail.com>
2015-11-19 17:03:31 +0300

    First steps

--- js/viewModel.js
+++ js/viewModel.js
@@ -1,3 +1,37 @@
 ko.applyBindings(new function () {
   var self = this;
+
+  self.posts = {
+    genDir: {
+      name: 'General director',
+      helpers: ko.observableArray(),
+      underPosts: ko.observableArray()
+    },
+
+    genDirHelper: {
+      name: 'Helper of general director'
+    },
+
+    employeeDir: {
+      name: 'Employee director',
+      helpers: ko.observableArray(),
+      underPosts: ko.observableArray()
+    },
+
+    ITDir: {
+      name: 'IT director',
+      helpers: ko.observableArray(),
+      underPosts: ko.observableArray()
+    }
+  };
+
+  self.posts.genDir.helpers([
+    self.posts.genDirHelper
+  ]);
+
+  self.posts.genDir.underPosts([
+    self.posts.employeeDir,
+    self.posts.ITDir
+  ]);
+
 });
